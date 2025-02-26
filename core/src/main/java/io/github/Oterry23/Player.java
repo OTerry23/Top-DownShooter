1252f3da6dc4110a518a9d8df63d784571efb09b
Owen Terry <oterry2323@gmail.com>
2025-02-26 15:06:01 -0600

    Removed mouse-look in favor for tank turning

--- core/src/main/java/io/github/Oterry23/Player.java
+++ core/src/main/java/io/github/Oterry23/Player.java
@@ -13,24 +13,18 @@ public class Player extends Actor{
     Sprite sprite;
     Texture texture;
 
-    Vector2 position;
-    Vector2 velocity;
     Vector2 direction;
     float rotation;
     
-    private boolean isDashing = false;
-    private final float speed = 100f;
-    private final float dashStrength = 10f;
+    private final float speed = 80f;
+    private final float rotationSpeed = 100f;
 
 
     public Player(float x, float y){
         texture = new Texture(Gdx.files.internal("placeholderPlayer.png"));
         sprite = new Sprite(texture, texture.getWidth(), texture.getHeight());
         
-        position = new Vector2(x, y);
-        velocity = new Vector2(0, 0);
-
-        setPosition(position.x, position.y);
+        setPosition(x, y);
     }
 
     @Override
@@ -51,52 +45,24 @@ public class Player extends Actor{
         sprite.draw(batch);
     }
 
-    public Vector2 getDirection(){
-        return direction;
-    }
-
-    public void rotate(Vector2 direction){
-        rotation = direction.angleDeg();
-        sprite.setRotation(rotation);
-    }
-
     public void move(float delta){
-        direction = new Vector2(Gdx.input.getX() - getX(), Gdx.graphics.getHeight() - Gdx.input.getY() - getY());
-        rotate(direction);
 
-        velocity.set(0, 0);
-
-        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)){
-            isDashing = true;
-        }
-
-        float currentSpeed = speed;
-
-        if (isDashing) {
-            currentSpeed *= dashStrength;
-        }
-
-
+        float angleRad = (float) Math.toRadians(sprite.getRotation());
+        direction = new Vector2((float) Math.cos(angleRad),(float) Math.sin(angleRad));
+        float dx = direction.x * speed * delta;
+        float dy = direction.y * speed * delta;
 
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-            direction.nor().scl(currentSpeed * delta);
-            setPosition(getX() + direction.x, getY() + direction.y);
+            setPosition(getX() + dx, getY() + dy);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-            direction.nor().scl(currentSpeed * delta);
-            setPosition(getX() - direction.x, getY() - direction.y);
+            setPosition(getX() - dx/2, getY() - dy/2);        
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) { // Strafe right
-            Vector2 right = new Vector2(direction.y, -direction.x).nor().scl(currentSpeed * delta);
-            setPosition(getX() + right.x, getY() + right.y);
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) { // Turn right
+            sprite.rotate(-rotationSpeed * delta);
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) { // Strafe left
-            Vector2 left = new Vector2(-direction.y, direction.x).nor().scl(currentSpeed * delta);
-            setPosition(getX() + left.x, getY() + left.y);
-        }
-
-        if (isDashing) {
-           isDashing = false;
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) { // Turn left
+            sprite.rotate(rotationSpeed * delta);
         }
     }
 
@@ -107,7 +73,6 @@ public class Player extends Actor{
             projectiles = new Projectiles();
         }
         if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)){
-            System.out.print(getStage());
             proj = new Projectile(getX(), getY(), direction);
             getStage().addActor(proj);
         }
